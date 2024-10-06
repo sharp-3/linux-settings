1c6ee4e7a6ce6193a9946bd544d1611fd2a974db
sharp-3 <ohagi.051108@gmail.com>
2024-10-07 04:42:45 +0900

    settings lazy.nvim -> dpp.vim

---
+++ .config/nvim/dpp.ts
@@ -0,0 +1,153 @@
+import {
+	type ContextBuilder,
+	type ExtOptions,
+	type Plugin,
+} from "jsr:@shougo/dpp-vim@~3.0.0/types";
+import {
+	BaseConfig,
+	type ConfigReturn,
+	type MultipleHook,
+} from "jsr:@shougo/dpp-vim@~3.0.0/config";
+import {
+	Protocol
+} from "jsr:@shougo/dpp-vim@~3.0.0/protocol";
+import {
+	mergeFtplugins
+} from "jsr:@shougo/dpp-vim@~3.0.0/utils";
+import type {
+	Ext as LazyExt,
+	LazyMakeStateResult,
+	Params as LazyParams,
+} from "jsr:@shougo/dpp-ext-lazy@~1.5.0";
+import type {
+  Ext as TomlExt,
+  Params as TomlParams,
+} from "jsr:@shougo/dpp-ext-toml@~1.3.0";
+import type { Denops } from "jsr:@denops/std@~7.2.0";
+import * as fn from "jsr:@denops/std@~7.2.0/function";
+import { globals } from "jsr:@denops/std@~7.2.0/variable"
+import { expandGlob } from "jsr:@std/fs@~1.0.0/expand-glob";
+
+export class Config extends BaseConfig {
+	override async config(args: {
+		denops: Denops;
+		contextBuilder: ContextBuilder;
+		basePath: string;
+	}): Promise<ConfigReturn> {
+		args.contextBuilder.setGlobal({
+			protocols: ["git"],
+			protocolParams: {
+				git: {
+					enablePartialClone: true,
+				},
+			},
+			extParams: {
+				installer: {
+					checkDiff: false,
+				},
+			},
+		});
+		
+		const [context, options] = await args.contextBuilder.get(args.denops);
+		const protocols = await args.denops.dispatcher.getProtocols() as Record<
+			string,
+			Protocol
+		>;
+		
+		const config = await globals.get(args.denops, "config")
+		const cache = await globals.get(args.denops, "cache")
+
+		const recordPlugins: Record<string, Plugin> = {};
+		const ftplugins: Record<string, string> = {};
+		const hooksFiles: string[] = [];
+		let multipleHooks: MultipleHook[] = [];
+
+		const [tomlExt, tomlOptions, tomlParams]: [
+			TomlExt | undefined,
+			ExtOptions,
+			TomlParams,
+		] = await args.denops.dispatcher.getExt(
+			"toml",
+		) as [TomlExt | undefined, ExtOptions, TomlParams];
+		if (tomlExt) {
+			const tomlPromises = [
+				{ path: `${config}/dpp.toml`, lazy: false },
+				{ path: `${config}/ddc.toml`, lazy: true },
+				{ path: `${config}/theme.toml`, lazy: true },
+				{ path: `${config}/denops.toml`, lazy: true },
+			].map((tomlFile) => 
+			     tomlExt.actions.load.callback({
+					denops: args.denops,
+					context,
+					options,
+					protocols,
+					extOptions: tomlOptions,
+					extParams: tomlParams,
+					actionParams: {
+						path: tomlFile.path,
+						options: {
+							lazy: tomlFile.lazy,
+						},
+					},
+			     })
+			);
+
+			const tomls = await Promise.all(tomlPromises);
+
+			for (const toml of tomls) {
+				for (const plugin of toml.plugins ?? []) {
+					recordPlugins[plugin.name] = plugin;
+				}
+				if (toml.ftplugins) {
+					mergeFtplugins(ftplugins, toml.ftplugins);
+				}
+				if (toml.multiple_hooks) {
+					multipleHooks = multipleHooks.concat(toml.multiple_hooks);
+				}
+				if (toml.hooks_file) {
+					hooksFiles.push(toml.hooks_file);
+				}
+			}
+		}
+
+		const [lazyExt, lazyOptions, lazyParams]: [
+			LazyExt | undefined,
+			ExtOptions,
+			LazyParams,
+		] = await args.denops.dispatcher.getExt(
+			"lazy",
+		) as [LazyExt | undefined, ExtOptions, LazyParams];
+		let lazyResult: LazyMakeStateResult | undefined = undefined;
+		if (lazyExt) {
+			lazyResult = await lazyExt.actions.makeState.callback({
+				denops: args.denops,
+				context,
+				options,
+				protocols,
+				extOptions: lazyOptions,
+				extParams: lazyParams,
+				actionParams: {
+					plugins: Object.values(recordPlugins),
+				},
+			});
+		}
+
+		const checkFiles = [];
+		for await (const file of expandGlob(`${config}/*`)) {
+			checkFiles.push(file.path);
+		}
+
+		console.log(checkFiles);
+		console.log(lazyResult?.plugins ?? [])
+
+		return {
+			checkFiles,
+			ftplugins,
+			hooksFiles,
+			multipleHooks,
+			plugins: lazyResult?.plugins ?? [],
+			stateLines: lazyResult?.stateLines ?? [],
+		};
+		
+	}
+}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,94 +1,105 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.g["loaded_python3_provider"] = 0
+vim.g["loaded_perl_provider"] = 0
+vim.g["loaded_ruby_provider"] = 0
+vim.g["loaded_node_provider"] = 0
 
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
+vim.g["config"] = vim.fn.expand("~/.config/nvim")
+vim.g["cache"] = vim.fn.expand("~/.cache/nvim")
+vim.g["denops#debug"] = 1
+
+function DppInitPlugin(plugin)
+	local src = vim.g["cache"] .. "/dpp/repos/github.com/" .. plugin
+	if not vim.loop.fs_stat(src) then
+		vim.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/" .. plugin .. ".git",
+			src,
+		})
+	end
+	vim.opt.runtimepath:prepend(src)
+end
+
+DppInitPlugin("Shougo/dpp.vim")
+DppInitPlugin("Shougo/dpp-ext-lazy")
+
+local dpp = require("dpp")
+
+local dpp_base = vim.g["cache"] .. "/dpp"
+local dpp_config = vim.g["config"] .. "/dpp.ts"
+
+function DppMakeState()
+	dpp.make_state(dpp_base, dpp_config)
+end
+
+
+if dpp.load_state(dpp_base) then
+	local plugins = {
+		"Shougo/dpp-ext-installer",
+		"Shougo/dpp-ext-local",
+		"Shougo/dpp-ext-toml",
+		"Shougo/dpp-protocol-git",
+		"vim-denops/denops.vim",
+	}
+	for i = 1, #plugins do
+		DppInitPlugin(plugins[i])
+	end
+	vim.fn["denops#server#wait_async"](function()
+		vim.api.nvim_echo({{"dpp.load_state() is failed.", "WarningMsg"}}, true, {})
+		DppMakeState()
+	end)
+else
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		pattern = { "*.lua", "*.vim", "*.toml", "*.ts", "vimrc", ".vimrc" },
+		callback = function()
+			dpp.check_files()
+		end
+	})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		pattern = { "*.toml" },
+		callback = function()
+			if 0 < #dpp.sync_ext_action("installer", "getNotInstalled") then
+				dpp.async_ext_action("installer", "install")
+			end
+		end
 	})
 end
 
-vim.opt.rtp:prepend(lazypath)
+vim.api.nvim_create_autocmd("User", {
+	pattern = "Dpp:makeStatePost",
+	callback = function()
+		vim.api.nvim_echo({{"dpp.make_state() is done."}}, true, {})
+	end
+})
 
--- provider disable
-vim.g['loaded_python3_provider'] = 0
-vim.g['loaded_ruby_provider'] = 0
-vim.g['loaded_node_provider'] = 0
-vim.g['loaded_perl_provider'] = 0
+vim.opt["fileformat"] = "unix"
+vim.opt["fileencodings"] = { "utf-8", "sjis", "cp932" }
+vim.opt["confirm"] = true
+vim.opt["number"] = true
+vim.opt["numberwidth"] = 6
+vim.opt["title"] = true
+vim.opt["titlestring"] = "%f"
+vim.opt["mouse"] = "a"
+vim.opt["wrap"]= false
+vim.opt["list"] = true
+vim.opt["listchars"] = { tab = "|-", eol = "$", precedes = "<", extends = ">" }
+vim.opt["tabstop"] = 4
+vim.opt["shiftwidth"] = 4
+vim.opt["softtabstop"] = 0
+vim.opt["expandtab"] = false
+vim.opt["virtualedit"] = "onemore"
+vim.opt["incsearch"] = true
+vim.opt["hlsearch"] = true
+vim.opt["ignorecase"] = true
+vim.opt["wrapscan"] = true
+vim.opt["syntax"] = "on"
+vim.cmd("filetype indent plugin on")
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true })
+vim.keymap.set("i", "==", ":LspDocumentFormat<CR>", { noremap = true })
 
--- file
-vim.opt.binary         = false
-vim.opt.fileformat     = "unix"
-vim.opt.fileignorecase = false
-vim.opt.fileencodings = { 'utf-8', 'sjis', 'cp932' }
---vim.opt.fileencoding  = ""
---vim.opt.encoding      = ""
--- save
-vim.opt.confirm        = true
--- line number
-vim.opt.number         = true
-vim.opt.numberwidth    = 6
--- title
-vim.opt.title          = true
-vim.opt.titlestring    = "%f"
--- cmd
-vim.opt.showcmd        = true
--- tab char
-vim.opt.tabstop        = 4
-vim.opt.shiftwidth     = 4
-vim.opt.softtabstop    = 0
-vim.opt.expandtab      = false
--- indent
-vim.opt.autoindent     = true
-vim.opt.smartindent    = true
--- visual
-vim.opt.ambiwidth      = "double"
-vim.opt.wrap           = false
-vim.opt.list           = true
-vim.opt.listchars      = { tab = '|-', eol = '$', precedes = '<', extends = '>' }
-vim.opt.virtualedit    = "onemore"
-vim.opt.showmatch      = true
-vim.opt.matchpairs     = { "(:)", "{:}", "[:]" }
-vim.opt.showmode       = true
--- status line
-vim.opt.laststatus     = 2
-vim.opt.statusline     = "%F[%R%M] %= %4l,%4c 0x%04B"
--- tab line
-vim.opt.showtabline    = 0
--- vim.opt.tabline        = ""
--- cursor
-vim.opt.cursorline     = true
-vim.opt.cursorcolumn   = false
-vim.opt.signcolumn     = "yes" --?
--- mouse
-vim.opt.mouse          = "a"
--- search
-vim.opt.incsearch      = true
-vim.opt.hlsearch       = true
-vim.opt.ignorecase     = true
-vim.opt.smartcase      = false --ignorecaseと逆
-vim.opt.wrapscan       = true
--- complement
-vim.opt.wildmenu       = true
-vim.opt.wildmode       = "longest"
-vim.opt.wildoptions    = ""
-vim.opt.wildignorecase = true
--- clipboard
-if vim.fn.has('wsl') == 1 then
-        vim.opt.clipboard = "unnamedplus"
-        -- apt install xclip
-elseif vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 or vim.fn.has('mac') == 1 then
-        vim.opt.clipboard = "unnamed"
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 or vim.fn.has("mac") == 1 then
+	vim.opt.clipboard = "unnamed"
 else
-        vim.opt.clipboard = "unnamedplus"
+	vim.opt.clipboard = "unnamedplus"
 end
-
--- keymap
--- vim.g.mapleader = vim.keycode'<Space>'
-vim.keymap.set('n','<Esc><Esc>', ':nohlsearch<CR>', { noremap = false })
-vim.keymap.set('n','==','gg=G',{noremap = true})
-
-require("lazy").setup("plugins")
